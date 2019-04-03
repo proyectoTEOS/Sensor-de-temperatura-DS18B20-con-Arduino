50249c5f10bcd2bb2c32d53f82045b2633611539
TEOS <proyectoteos@gmail.com>
2019-04-03 01:25:13 -0600

    Code and scheme available.

---
+++ Sensor-de-temperatura-DS18B20-con-Arduino.ino
@@ -0,0 +1,30 @@
+/*
+  Created by TEOS
+  Domotic with Arduino https://goo.gl/btZpjs
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  WEB https://www.proyecto-teos.com
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+const uint8_t dsPinT = 2;
+
+OneWire wireT(dsPinT);
+DallasTemperature dallasT(&wireT);
+
+void setup() {
+  dallasT.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  dallasT.requestTemperatures();
+  String resultSerialT = "Temperatura actual: " + String(dallasT.getTempCByIndex(0)) + " °C";
+  Serial.print(resultSerialT);
+  delay(200);
+}
